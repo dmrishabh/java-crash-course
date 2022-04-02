4a9df4c22bb07164fd3a171208519ad8e64b394f
dmrishabh <mishrarishabh2462@gmail.com>
2022-04-02 12:35:37 +0530

    string method

---
+++ replace.java
@@ -0,0 +1,20 @@
+public class replace {
+
+    // we have a name Aanaisha and we've to convert a into * means A*n*ish*
+    public static void main(String[] args) {
+        // replace
+        String name = "Aanaisha";
+
+        System.out.println(name);
+        System.out.println(name.replace('a', '*'));
+
+        // toUppercase
+
+        System.out.println(name.toUpperCase());
+        // toLowercase
+        
+        System.out.println(name.toLowerCase());
+    }
+
+}
+ 
